918ab43d06f22476738834d7433eac732e4b175f
teerawat2627 <teerawat2224@gmail.com>
2021-12-26 00:41:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var img = document.getElementById("popcat1");
+    var count = document.getElementById("score");
+    var score = 0;
+    var audio = new Audio("pop.mp3");
+
+    img.addEventListener('mousedown',function(){
+        increaseScore();
+        img.src = 'popcat2.png';
+    });
+
+    img.addEventListener('mouseup',function(){
+        img.src = 'popcat1.png';
+});
+
+    function increaseScore(){
+        score++;
+        count.innerHTML = score;
+    }
+}
